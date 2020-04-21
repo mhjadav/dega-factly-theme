a398d142bc67424f1bc3e1ba11e38d2ccfc457a9
Mahipat <mj@mojaave.com>
2020-04-21 17:50:56 +0530

    Added animation on page load

--- src/components/layout.js
+++ src/components/layout.js
@@ -52,7 +52,10 @@ const Layout = props => (
             link={[{ rel: 'canonical', href: siteMetadata.siteUrl }]}
           >
             <html lang={siteMetadata.lang} />
-            <body className="bg-white text-gray-900 leading-normal mx-auto w-full max-w-screen-xl "></body>
+            <body
+              className="bg-white text-gray-900 leading-normal mx-auto w-full max-w-screen-xl fadeInUp"
+              style={{ animationDelay: `${1 + 1 * 0.1}s` }}
+            ></body>
           </Helmet>
           <Navbar></Navbar>
           {children}
