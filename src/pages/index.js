36b2d3537d657b3938b5915621a8d6a67faf7808
Mahipat <mj@mojaave.com>
2020-04-21 18:59:22 +0530

    Fixed infinite scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ const items = [
   }
 ];
 
-const posts = Array(1000).fill(0);
+const posts = Array(1000).fill(12);
 const IndexPage = ({ data }) => {
   const [sidebarDockedBottom, setSidebarDockedBottom] = useState('');
   const [postItems, setPostItems] = useState(posts.slice(0, 6));
@@ -91,7 +91,8 @@ const IndexPage = ({ data }) => {
   const sidebarElement = useRef(null);
   let prevScrollPosition = 0;
   const isBottom = el =>
-    sidebarElement.current.getBoundingClientRect().bottom <= window.innerHeight;
+    sidebarElement.current.getBoundingClientRect().bottom <=
+    window.innerHeight + 5;
 
   const trackScrolling = useCallback(() => {
     const sidebarTop = Math.abs(
@@ -140,138 +141,143 @@ const IndexPage = ({ data }) => {
 
   return (
     <Layout>
-      <InfiniteScroll
-        pageStart={0}
-        loadMore={handleLoadMore}
-        hasMore={hasNextPage}
-      >
-        <div className="w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t mx-auto px-6 pt-32">
-          {/* Lead Card */}
-          <div
-            className="flex flex-col lg:flex-row justify-between pt-12 pb-6 border-b fadeInUp"
-            style={{ animationDelay: `${1 + 1 * 0.1}s` }}
-          >
-            <div className="flex flex-wrap justify-center lg:w-3/5 bg-white rounded overflow-hidden">
+      <div className="w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t mx-auto px-6 pt-32">
+        {/* Lead Card */}
+        <div
+          className="flex flex-col lg:flex-row justify-between pt-12 pb-6 border-b fadeInUp"
+          style={{ animationDelay: `${1 + 1 * 0.1}s` }}
+        >
+          <div className="flex flex-wrap justify-center lg:w-3/5 bg-white rounded overflow-hidden">
+            <a
+              href="/"
+              className="w:full flex flex-col flex-1 no-underline hover:no-underline px-2 mb-4 shadow-lg sm:shadow-none"
+            >
+              <div className="w-full rounded-t">
+                <img
+                  src={`https://source.unsplash.com/collection/${2 +
+                    1000}/800x200`}
+                  alt={2}
+                  className="w-full shadow"
+                />
+              </div>
+              <div className="relative">
+                <div className="w-full flex-1 bg:white sm:bg-black opacity-50 static sm:absolute bottom-0 overflow-hidden">
+                  <div className="w-full font-bold text-base text-base sm:text-white px-4 py-2">
+                    👋 Welcome fellow Tailwind CSS and Ghost fan
+                  </div>
+                </div>
+              </div>
+            </a>
+            {[1, 2].map(item => (
               <a
                 href="/"
-                className="w:full flex flex-col flex-1 no-underline hover:no-underline px-2 mb-4 shadow-lg sm:shadow-none"
+                className="w:full flex flex-row flex-wrap sm:flex-no-wrap no-underline hover:no-underline px-2 mb-4"
               >
-                <div className="w-full rounded-t">
+                <div className="sm:w-1/4 flex justify-center">
                   <img
-                    src={`https://source.unsplash.com/collection/${2 +
+                    src={`https://source.unsplash.com/collection/${item +
                       1000}/800x200`}
-                    alt={2}
-                    className="w-full shadow"
+                    alt={item}
+                    className="h-32 rounded object-cover"
                   />
                 </div>
-                <div className="relative">
-                  <div className="w-full flex-1 bg:white sm:bg-black opacity-50 static sm:absolute bottom-0 overflow-hidden">
-                    <div className="w-full font-bold text-base text-base sm:text-white px-4 py-2">
-                      👋 Welcome fellow Tailwind CSS and Ghost fan
+                <div className="sm:w-3/4 flex flex-col py-4 sm:py-0 sm:px-4">
+                  <div className="w-full font-bold font-sans text-base text-gray-800 pb-4">
+                    Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                    Lorem ipsum dolor sit amet.
+                  </div>
+                  <p className="text-gray-800 font-sans text-sm">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                  <div className="flex-none mt-auto py-4 bg-white">
+                    <div className="flex items-center justify-between">
+                      <div className="flex justify-center items-center">
+                        <img
+                          className="w-8 h-8 rounded-full mr-2 avatar"
+                          data-tippy-content="Author Name"
+                          src="http://i.pravatar.cc/300"
+                          alt="Avatar of Author"
+                        />
+                        <span className="text-gray-600 text-xs md:text-sm">
+                          John Doe
+                        </span>
+                      </div>
+                      <p className="text-gray-600 text-xs md:text-sm">
+                        1 MIN READ
+                      </p>
                     </div>
                   </div>
                 </div>
               </a>
-              {[1, 2].map(item => (
-                <a
-                  href="/"
-                  className="w:full flex flex-row flex-wrap sm:flex-no-wrap no-underline hover:no-underline px-2 mb-4"
-                >
-                  <div className="sm:w-1/4 flex justify-center">
-                    <img
-                      src={`https://source.unsplash.com/collection/${item +
-                        1000}/800x200`}
-                      alt={item}
-                      className="h-32 rounded object-cover"
-                    />
-                  </div>
-                  <div className="sm:w-3/4 flex flex-col py-4 sm:py-0 sm:px-4">
-                    <div className="w-full font-bold font-sans text-base text-gray-800 pb-4">
-                      Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-                      Lorem ipsum dolor sit amet.
-                    </div>
-                    <p className="text-gray-800 font-sans text-sm">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </p>
-                    <div className="flex-none mt-auto py-4 bg-white">
-                      <div className="flex items-center justify-between">
-                        <div className="flex justify-center items-center">
-                          <img
-                            className="w-8 h-8 rounded-full mr-2 avatar"
-                            data-tippy-content="Author Name"
-                            src="http://i.pravatar.cc/300"
-                            alt="Avatar of Author"
-                          />
-                          <span className="text-gray-600 text-xs md:text-sm">
-                            John Doe
-                          </span>
-                        </div>
-                        <p className="text-gray-600 text-xs md:text-sm">
-                          1 MIN READ
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              ))}
+            ))}
+          </div>
+          <div className="w-full p-0 lg:px-6 lg:w-2/5 flex flex-col overflow-hidden">
+            {items.slice(0, 4).map((item, index) => (
+              <ListItems item={item} index={index} />
+            ))}
+          </div>
+        </div>
+        {/* /Lead Card */}
+        {/* Posts Container */}
+        <div className="flex flex-wrap relative justify-between pt-6 -mx-6">
+          {/* 2/3 col */}
+          <div className="w-full lg:w-3/5 p-6 flex flex-col flex-grow flex-shrink border-b">
+            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden">
+              <a
+                href="#"
+                className="flex flex-wrap no-underline hover:no-underline"
+              >
+                <img
+                  src="https://source.unsplash.com/collection/345/800x600"
+                  className="h-full w-full rounded-t pb-6"
+                />
+                <p className="w-full text-gray-600 text-xs md:text-sm px-6">
+                  GETTING STARTED
+                </p>
+                <div className="w-full font-bold text-xl text-gray-900 px-6">
+                  Lorem ipsum dolor sit amet.
+                </div>
+                <p className="text-gray-800 font-sans text-base px-6 mb-5">
+                  Lorem ipsum eu nunc commodo posuere et sit amet ligula.
+                </p>
+              </a>
             </div>
-            <div className="w-full p-0 lg:px-6 lg:w-2/5 flex flex-col overflow-hidden">
-              {items.slice(0, 4).map((item, index) => (
-                <ListItems item={item} index={index} />
-              ))}
+            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden p-6">
+              <div className="flex items-center justify-between">
+                <img
+                  className="w-8 h-8 rounded-full mr-4 avatar"
+                  data-tippy-content="Author Name"
+                  src="http://i.pravatar.cc/300"
+                  alt="Avatar of Author"
+                />
+                <p className="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
+              </div>
             </div>
           </div>
-          {/* /Lead Card */}
-          {/* Posts Container */}
-          <div className="flex flex-wrap relative justify-between pt-6 -mx-6">
-            {/* 2/3 col */}
-            <div className="w-full lg:w-3/5 p-6 flex flex-col flex-grow flex-shrink border-b">
-              <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden">
-                <a
-                  href="#"
-                  className="flex flex-wrap no-underline hover:no-underline"
-                >
-                  <img
-                    src="https://source.unsplash.com/collection/345/800x600"
-                    className="h-full w-full rounded-t pb-6"
-                  />
-                  <p className="w-full text-gray-600 text-xs md:text-sm px-6">
-                    GETTING STARTED
-                  </p>
-                  <div className="w-full font-bold text-xl text-gray-900 px-6">
-                    Lorem ipsum dolor sit amet.
-                  </div>
-                  <p className="text-gray-800 font-sans text-base px-6 mb-5">
-                    Lorem ipsum eu nunc commodo posuere et sit amet ligula.
-                  </p>
-                </a>
-              </div>
-              <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden p-6">
-                <div className="flex items-center justify-between">
-                  <img
-                    className="w-8 h-8 rounded-full mr-4 avatar"
-                    data-tippy-content="Author Name"
-                    src="http://i.pravatar.cc/300"
-                    alt="Avatar of Author"
-                  />
-                  <p className="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
+          {/* 1/3 col */}
+          <div className="w-full lg:w-2/5 p-6 flex flex-col flex-grow flex-shrink border-b">
+            <div className="mb-4 pb-2 text-xl border-b">
+              <h5 className="font-thin text-text-xl">Top In Factchecks</h5>
+            </div>
+            {items.slice(1, 6).map((item, index) => (
+              <ListItems item={item} index={index} image={false} />
+            ))}
+          </div>
+          <div
+            ref={contentElement}
+            className="w-full lg:w-3/5 order-2 lg:order-1 p-6 mt-6 flex flex-col flex-grow flex-shrink overflow-auto h-auto"
+          >
+            <InfiniteScroll
+              pageStart={0}
+              loadMore={handleLoadMore}
+              hasMore={hasNextPage}
+              loader={
+                <div className="loader" key={0}>
+                  Loading ...
                 </div>
-              </div>
-            </div>
-            {/* 1/3 col */}
-            <div className="w-full lg:w-2/5 p-6 flex flex-col flex-grow flex-shrink border-b">
-              <div className="mb-4 pb-2 text-xl border-b">
-                <h5 className="font-thin text-text-xl">Top In Factchecks</h5>
-              </div>
-              {items.slice(1, 6).map((item, index) => (
-                <ListItems item={item} index={index} image={false} />
-              ))}
-            </div>
-            <div
-              ref={contentElement}
-              className="w-full lg:w-3/5 order-2 lg:order-1 p-6 mt-6 flex flex-col flex-grow flex-shrink overflow-auto h-auto"
+              }
             >
               {postItems.map(item => (
                 <a
@@ -320,48 +326,42 @@ const IndexPage = ({ data }) => {
                   </div>
                 </a>
               ))}
-            </div>
-            <div className="w-full lg:w-2/5 flex flex-col flex-grow flex-shrink order-1 lg:order-2 lg:border-l-2 border-b-2 lg:border-b-0 p-6 mt-6">
-              <div ref={sidebarElement} className={`${sidebarDockedBottom}`}>
-                <div className="block">
-                  <div className="mb-4 pb-2 text-xl border-b">
-                    <h5 className="font-thin text-text-xl">
-                      Top In Factchecks
-                    </h5>
-                  </div>
-                  {items.slice(1, 6).map((item, index) => (
-                    <ListItems item={item} index={index} />
-                  ))}
+            </InfiniteScroll>
+          </div>
+          <div className="w-full lg:w-2/5 flex flex-col flex-grow flex-shrink order-1 lg:order-2 lg:border-l-2 border-b-2 lg:border-b-0 p-6 mt-6">
+            <div ref={sidebarElement} className={`${sidebarDockedBottom}`}>
+              <div className="block">
+                <div className="mb-4 pb-2 text-xl border-b">
+                  <h5 className="font-thin text-text-xl">Top In Factchecks</h5>
                 </div>
+                {items.slice(1, 6).map((item, index) => (
+                  <ListItems item={item} index={index} />
+                ))}
+              </div>
 
-                <div className="block">
-                  <div className="mb-4 pb-2 text-xl border-b">
-                    <h5 className="font-thin text-text-xl">
-                      Top In Factchecks
-                    </h5>
-                  </div>
-                  {items.slice(1, 6).map((item, index) => (
-                    <ListItems item={item} index={index} />
-                  ))}
+              <div className="block">
+                <div className="mb-4 pb-2 text-xl border-b">
+                  <h5 className="font-thin text-text-xl">Top In Factchecks</h5>
                 </div>
+                {items.slice(1, 6).map((item, index) => (
+                  <ListItems item={item} index={index} />
+                ))}
+              </div>
 
-                <div className="block">
-                  <div className="mb-4 pb-2 text-xl border-b">
-                    <h5 className="font-thin text-text-xl">
-                      Top In Factchecks
-                    </h5>
-                  </div>
-                  {items.slice(1, 6).map((item, index) => (
-                    <ListItems item={item} index={index} />
-                  ))}
-                  <Footer className="hidden lg:block"></Footer>
+              <div className="block">
+                <div className="mb-4 pb-2 text-xl border-b">
+                  <h5 className="font-thin text-text-xl">Top In Factchecks</h5>
                 </div>
+                {items.slice(1, 6).map((item, index) => (
+                  <ListItems item={item} index={index} />
+                ))}
+                <Footer className="hidden lg:block"></Footer>
               </div>
             </div>
           </div>
-          {/* / Post Content */}
         </div>
-      </InfiniteScroll>
+        {/* / Post Content */}
+      </div>
     </Layout>
   );
 };
